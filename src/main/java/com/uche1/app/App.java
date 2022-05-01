6213f1d4b6dc5dfb42cb86a20af1495d8e87c944
Green03360 <ucgreen238@gmail.com>
2022-04-30 23:39:44 -0400

    java-code

---
+++ src/main/java/com/uche1/app/App.java
@@ -0,0 +1,13 @@
+package com.uche1.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello WorldOur God is good!" );
+    }
+}
